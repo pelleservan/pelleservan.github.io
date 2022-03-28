36b9a844181b349c0586cb9def458c25f17c1612
pelleservan <97622610+pelleservan@users.noreply.github.com>
2022-03-28 10:27:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="indexstyle.css">
+	<title>HumanResourcesDashBoard</title>
+</head>
+
+<body>
+	<div id="page">
+		<?php include('header.php'); ?>
+		<div id="vid">
+			<video autoplay loop src="bat.mp4"></video>
+		</div>
+		<div id="foot">
+			<?php include('footer.php'); ?>
+		</div>
+	</div>
+</body>
+</html>  
